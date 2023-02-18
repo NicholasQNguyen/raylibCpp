7a52b65dcd51f0bf4571f4c61d9924969cee79ae
NicholasQNguyen <nicholasqnguyen8@gmail.com>
2023-02-17 23:41:13 -0500

    cleanup

--- library.h
+++ library.h
@@ -8,8 +8,7 @@ namespace library
 {
     int main()
     {
-        // Initialization
-    //--------------------------------------------------------------------------------------
+    // Initialization
     InitWindow(definitions::SCREEN_WIDTH,
                definitions::SCREEN_HEIGHT,
                "raylib [core] example - basic window");
@@ -17,30 +16,26 @@ namespace library
     definitions::turn currentTurn;
     currentTurn = definitions::player;
     character::Character player = character::Character("Sam", 50);
-    //--------------------------------------------------------------------------------------
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
         // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
         if (currentTurn == definitions::player)
         {
             std::cout << "DAB\n";
         }
-        //----------------------------------------------------------------------------------
+        else
+        {
+            std::cout << "WHIP\n";
+        }
         // Draw
-        //----------------------------------------------------------------------------------
         BeginDrawing();
             ClearBackground(RAYWHITE);
             DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
         EndDrawing();
-        //----------------------------------------------------------------------------------
     }
     // De-Initialization
-    //--------------------------------------------------------------------------------------
     CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
     return 0;
     }
 }
